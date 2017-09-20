2ab734057512a28581f6a6c90da253b5b7f1914c
Davis Lin <da860610@gmail.com>
2017-09-21 00:07:17 +0800

    test

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main(server='8.8.8.8', protocol='icmp', port='', count='3', output_path='./t
 
         data = json.loads(newprocess)
 
-        print(data[0])
+        print(data)
 
     if protocol == 'tcp':
         base_mtr_command += "--tcp "
--- test.py
+++ test.py
@@ -1,9 +1,22 @@
 import json
-
+from pprint import pprint
 
 with open('test.txt') as f:
     data = json.load(f)
-    ex = data['report']['hubs']
-    print(ex)
 
+data = dict(data)
+len = len(data['report']['hubs'])
+
+for i in range(0, len):
+    print(data['report']['hubs'][i]['count'])
+    print(data['report']['hubs'][i]['host'])
+    print(data['report']['hubs'][i]['ASN'])
+    print(data['report']['hubs'][i]['Snt'])
+    print(data['report']['hubs'][i]['Rcv'])
+    print(data['report']['hubs'][i]['Drop'])
+    print(data['report']['hubs'][i]['Loss%'])
+    print(data['report']['hubs'][i]['Avg'])
+    print(data['report']['hubs'][i]['Best'])
+    print(data['report']['hubs'][i]['Wrst'])
+    i += 1
 
