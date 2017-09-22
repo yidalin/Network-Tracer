280e9a21c22174edfbbf49483f48d33cf92ba5f0
Davis Lin <da860610@gmail.com>
2017-09-23 04:37:40 +0800

    test

--- pyenv.sh
+++ pyenv.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+yum install -y sqlite-devel
+
 yum install -y gcc zlib-devel.x86_64 bzip2.x86_64 bzip2-devel.x86_64 readline-devel.x86_64 openssl-devel.x86_64
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
 
