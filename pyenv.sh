e37cea13a60956e568e7a91f1597fa8b343512b2
Davis Lin <da860610@gmail.com>
2017-09-23 03:53:26 +0800

    Add pyenv.sh

---
+++ pyenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+
+exec "$SHELL"
+
+pyenv -V
