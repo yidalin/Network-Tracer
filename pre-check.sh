408c3004538876da62b6b46349ce67648e01ca89
Davis Lin <da860610@gmail.com>
2017-09-23 23:20:04 +0800

    Alpha version 0.9.1

--- pre-check.sh
+++ pre-check.sh
@@ -8,6 +8,30 @@ read choice
 
 if [ ${choice} -eq '1' ]; then
     echo "You choose wget"
+
 elif [ ${choice} -eq '2' ]; then
     echo "You choose mtr"
+
+    which mtr > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo -e ">> The command mtr exist."
+    else
+        echo -e ">> Could not found mtr..."
+        echo -e ">> Now installing mtr from yum repo"
+        #yum install -y mtr > /dev/null 2>&1 && ls -l $(which mtr)
+
+        if [ ! $(cat /etc/redhat-release | awk -F' ' '{print $1}') -eq 'CentOS' ]; then
+            exit
+        fi
+
+        yum install -y wget gcc autoconf
+
+        cd /root
+
+        wget ftp://ftp.bitwizard.nl/mtr/mtr-0.92.tar.gz -O ./mtr-0.92.tar.gz
+
+        tar zxvf ./mtr-0.92.tar.gz && cd ./mtr-0.92
+        ./configure && make && make install
+    fi
 fi
